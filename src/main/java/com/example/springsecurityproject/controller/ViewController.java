7a2393d284020811d32a905e429be9c86597a593
Daniel <bdcanop@eafit.edu.co>
2025-04-05 19:15:18 -0500

    Project initialization

---
+++ src/main/java/com/example/springsecurityproject/controller/ViewController.java
@@ -0,0 +1,28 @@
+package com.example.springsecurityproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
